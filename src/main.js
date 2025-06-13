a30bc3c2b5591247a01f0eab6bf96cd5c0325180
czy <408916854@qq.com>
2025-06-13 13:45:14 +0800

    begain

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus' // 引入 Element Plus
-import 'element-plus/dist/index.css' // 引入样式文
-import './style.css'
-import router from './router'
-
-createApp(App).use(ElementPlus).use(router).mount('#app')
